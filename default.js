fc7c5dfb09c75841c56c720a1cd13878a10621ba
Olivier Brückner <25103920+olivierbrcknr@users.noreply.github.com>
2020-10-26 20:01:32 +0100

    Add 7in7 entry 5

---
+++ default.js
@@ -0,0 +1,17 @@
+
+let checkImagesFor2x = () => {
+  let images = document.querySelectorAll('img');
+
+  images.forEach( (img) => {
+
+    if( img.src.includes('@2x') ){
+      img.onload = () => {
+        img.style.maxWidth = img.naturalWidth / 2 + 'px';
+      }
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+  checkImagesFor2x();
+});
